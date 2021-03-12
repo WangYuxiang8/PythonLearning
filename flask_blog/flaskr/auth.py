005448c022301358898e715e0cec1fd6b70b3d55
JosephWhang <615418349@qq.com>
2021-03-12 11:45:50 +0800

    add nested tuple

---
+++ flask_blog/flaskr/auth.py
@@ -0,0 +1,46 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from .db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+# 注册（register）
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?,?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+    return render_template('auth/register.html')
+
+# 登录（login）
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    return render_template('auth/login.html')
+
+# 注销（logout）
